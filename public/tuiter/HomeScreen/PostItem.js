b4cee4a2f2019926cbf1d6282f9ab61cb710f335
Rayzzzx <56136510+Rayzzzx@users.noreply.github.com>
2022-10-21 16:35:43 -0700

    finish challenge

--- public/tuiter/HomeScreen/PostItem.js
+++ public/tuiter/HomeScreen/PostItem.js
@@ -1,9 +1,11 @@
 const postItem = (post) => {
-    return (`
+  return `
         <li class="list-group-item">
             <div class="row">
                 <div class="col-1">
-                    <img class="rounded-circle" style="width: 45px" src=${post.avatarIcon}>
+                    <img class="rounded-circle" style="width: 45px" src=${
+                      post.avatarIcon
+                    }>
                 </div>
                 <div class="col-11">
                     <div class="row">
@@ -11,7 +13,9 @@ const postItem = (post) => {
                             <div class="col-11">
                                 <span class="fw-bolder">${post.userName}</span> 
                                 <i class="fa-solid fa-circle-check"></i>
-                                <span class="text-secondary"> @${post.handle} - ${post.time}</span>
+                                <span class="text-secondary"> @${
+                                  post.handle
+                                } - ${post.time}</span>
                             </div>
                             <div class="col-1 pe-0">
                                 <span class="float-end text-secondary">...</span>
@@ -22,23 +26,44 @@ const postItem = (post) => {
                     
                     <div class="card mt-2 border-secondary rounded-4">
                       <img src=${post.image} class="rounded-4">
-                      ${post.topicTitle || post.topicContent || post.topicLink ?
-                        `<div class="card-body border-top border-secondary p-3">
-                            ${post.topicTitle ? `<p class="card-text mb-0">${post.topicTitle}</p>` : ``}
-                            ${post.topicContent ? `<p class="text-secondary mb-0">${post.topicContent}</p>` : ``}
-                            ${post.topicContent ? `<p class="text-secondary mb-0"><i class="fa-solid fa-link"></i>${post.topicLink}</p>` : ``}
-                          </div>` : ``}
+                      ${
+                        post.topicTitle || post.topicContent || post.topicLink
+                          ? `<div class="card-body border-top border-secondary p-3">
+                            ${
+                              post.topicTitle
+                                ? `<p class="card-text mb-0">${post.topicTitle}</p>`
+                                : ``
+                            }
+                            ${
+                              post.topicContent
+                                ? `<p class="text-secondary mb-0">${post.topicContent}</p>`
+                                : ``
+                            }
+                            ${
+                              post.topicContent
+                                ? `<p class="text-secondary mb-0"><i class="fa-solid fa-link"></i>${post.topicLink}</p>`
+                                : ``
+                            }
+                          </div>`
+                          : ``
+                      }
                     </div>
                     
                     <div class="row mt-2">
                         <div class="col-3">
-                            <a class="text-secondary text-decoration-none" href="#"><i class="fa-regular fa-comment"></i><span class="ms-3">${post.comment}</span></a>
+                            <a class="text-secondary text-decoration-none" href="#"><i class="fa-regular fa-comment"></i><span class="ms-3">${
+                              post.comment
+                            }</span></a>
                         </div>
                         <div class="col-3">
-                            <a class="text-secondary text-decoration-none" href="#"><i class="fa-solid fa-retweet"></i><span class="ms-3">${post.share}</span></a>
+                            <a class="text-secondary text-decoration-none" href="#"><i class="fa-solid fa-retweet"></i><span class="ms-3">${
+                              post.share
+                            }</span></a>
                         </div>
                         <div class="col-3">
-                            <a class="text-secondary text-decoration-none" href="#" class="wd-liked"><i class="fa-solid fa-heart"></i><span class="ms-3">${post.like}</span></a>
+                            <a class="text-secondary text-decoration-none" href="#" class="wd-liked"><i class="fa-solid fa-heart"></i><span class="ms-3">${
+                              post.like
+                            }</span></a>
                         </div>
                         <div class="col-3">
                             <a class="text-secondary text-decoration-none" href="#"><i class="fa-solid fa-arrow-up-from-bracket"></i></a>
@@ -49,7 +74,7 @@ const postItem = (post) => {
             
             </div>
         </li>
-    `)
-}
+    `;
+};
 
 export default postItem;
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,49 +1,108 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
 
-const NavigationSidebar = (
-    active
-) => {
-    return (
-        <div className="list-group">
-            <Link className="list-group-item list-group-item-action"><i
-                className="fa-solid fa-t"></i></Link>
+const NavigationSidebar = () => {
+  const { pathname } = useLocation();
+  const paths = pathname.split("/");
+  const active = paths[2];
 
-            <a className={`list-group-item list-group-item-action ${active === 'home' ? 'active'
-                                                                                      : ''}`}>
-                <i className="fa-solid fa-house"></i>
-                <span className="d-none d-xxl-block wd-margin-left">Home</span>
-            </a>
-
-            <a className={`list-group-item
-                    ${active === 'explore' ? 'active' : ''}`}>
-                Explore
-            </a>
-            <a className={`list-group-item
-                    ${active === 'notifications' ? 'active' : ''}`}>
-                Notifications
-            </a>
-            <a className={`list-group-item
-                    ${active === 'messages' ? 'active' : ''}`}>
-                Messages
-            </a>
-            <a className={`list-group-item
-                    ${active === 'bookmarks' ? 'active' : ''}`}>
-                Bookmarks
-            </a>
-            <a className={`list-group-item
-                    ${active === 'lists' ? 'active' : ''}`}>
-                Lists
-            </a>
-            <a className={`list-group-item
-                    ${active === 'profile' ? 'active' : ''}`}>
-                Profile
-            </a>
-            <a className={`list-group-item
-                    ${active === 'more' ? 'active' : ''}`}>
-                More
-            </a>
+  return (
+    <div className="list-group">
+      <Link className="list-group-item">
+        <i className="fa-solid fa-t"></i>
+      </Link>
+      <Link
+        to="/tuiter/home"
+        className={`list-group-item
+                    ${active === "home" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fas fa-home"></i>
         </div>
-    );
+        <div className="d-none float-start d-xl-block">&nbsp;Home</div>
+      </Link>
+      <Link
+        to="/"
+        className={`list-group-item
+                    ${active === "lab" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fa-solid fa-flask-vial"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;Labs</div>
+      </Link>
+      <Link
+        to="/tuiter/explore"
+        className={`list-group-item
+                    ${active === "explore" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;Explore</div>
+      </Link>
+      <Link
+        to="/"
+        className={`list-group-item
+                    ${active === "notifications" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fas fa-bell"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;Notifications</div>
+      </Link>
+      <Link
+        to="/"
+        className={`list-group-item
+                    ${active === "messages" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fas fa-envelope"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;Messages</div>
+      </Link>
+      <Link
+        to="/"
+        className={`list-group-item
+                    ${active === "bookmarks" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fas fa-bookmark"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;Bookmarks</div>
+      </Link>
+      <Link
+        to="/"
+        className={`list-group-item
+                    ${active === "lists" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fas fa-list"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;Lists</div>
+      </Link>
+      <Link
+        to="/"
+        className={`list-group-item
+                    ${active === "profile" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fas fa-user"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;Profile</div>
+      </Link>
+      <Link
+        to="/"
+        className={`list-group-item
+                    ${active === "more" ? "active" : ""}`}
+      >
+        <div className="float-start">
+          <i className="fa-solid fa-circle-info"></i>
+        </div>
+        <div className="d-none float-start d-xl-block">&nbsp;More</div>
+      </Link>
+    </div>
+  );
 };
 export default NavigationSidebar;
