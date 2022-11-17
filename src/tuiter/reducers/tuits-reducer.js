6b6a94c273145693841b27808b4075096f85c223
Rayzzzx <56136510+Rayzzzx@users.noreply.github.com>
2022-11-16 20:46:55 -0800

    finish

--- src/tuiter/reducers/tuits-reducer.js
+++ src/tuiter/reducers/tuits-reducer.js
@@ -1,5 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import tuits from "../data/tuits.json";
+import {
+  findTuitsThunk,
+  deleteTuitThunk,
+  createTuitThunk,
+  updateTuitThunk,
+} from "../../services/tuits-thunks";
 
 const currentUser = {
   userName: "NASA",
@@ -11,28 +16,50 @@ const templateTuit = {
   ...currentUser,
   topic: "Space",
   time: "2h",
-  liked: false,
+  // liked: false,
   replies: 0,
   retuits: 0,
-  likes: 0,
+  // likes: 0,
+};
+
+const initialState = {
+  tuits: [],
+  loading: false,
 };
 
 const tuitSlice = createSlice({
   name: "tuits",
-  initialState: tuits,
-  reducers: {
-    createTuit(state, action) {
-      state.unshift({
-        ...action.payload,
-        ...templateTuit,
-        _id: new Date().getTime(),
-      });
+  initialState,
+  extraReducers: {
+    [findTuitsThunk.pending]: (state) => {
+      state.loading = true;
+      state.tuits = [];
     },
-    deleteTuit(state, action) {
-      const index = state.findIndex((tuit) => tuit._id === action.payload);
-      state.splice(index, 1);
+    [findTuitsThunk.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.tuits = payload;
+    },
+    [findTuitsThunk.rejected]: (state) => {
+      state.loading = false;
+    },
+    [deleteTuitThunk.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.tuits = state.tuits.filter((t) => t._id !== payload);
+    },
+    [createTuitThunk.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.tuits.push({ ...payload, ...templateTuit });
+    },
+    [updateTuitThunk.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      const tuitNdx = state.tuits.findIndex((t) => t._id === payload._id);
+      state.tuits[tuitNdx] = {
+        ...state.tuits[tuitNdx],
+        ...payload,
+      };
     },
   },
+  reducers: {},
 });
 
 export const { createTuit, deleteTuit } = tuitSlice.actions;
